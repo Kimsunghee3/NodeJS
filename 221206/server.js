829a7156f23ff607db9330fce853469bcb7178bc
chopssal <koalasunghee@gmail.com>
2023-05-28 19:54:35 +0900

    221207

--- 221206/server.js
+++ 221206/server.js
@@ -3,5 +3,20 @@
 const express = require("express")
 
 // express를 실행시켜서 나온 결과물을 app변수에 담겠다.
-// app은 객체이다.
+// app은 객체이고, return값도 객체이다.
 const app = express()
+
+// form엘리먼트의 get, post라고 생각하면된다.
+// req: 요청, res:응답
+app.get("/", (req,res) => {
+    console.log(req.query)
+    res.send('hello server')
+})
+
+// 3000번 포트를 사용하여 시작하고, 프로세스를 올리면 콜백함수가 실행되면서
+// 콘솔이 찍히게끔 하겠다.
+app.listen(3000, () => {
+    console.log("express server")
+})
+
+
