324319d015aded7b49fdfafeb8f489e9f3991768
chopssal <koalasunghee@gmail.com>
2023-05-29 18:38:16 +0900

    buffer.from()

--- 221207/server.js
+++ 221207/server.js
@@ -2,6 +2,8 @@ const net = require("net")
 const PORT = process.env.SERVER_PORT || 3000
 const HOST = process.env.SERVER_PORT || "127.0.0.1"
 
+const body = Buffer.from(`<h1>Hello world!!</h1>`)
+
 const server = net.createServer((client) => {
     client.on("data", (data) => {
         console.log(data)
@@ -21,4 +23,5 @@ server.listen(PORT, HOST, () => {
         console.log("client가 접속함!")
     })
 }) 
+
 
